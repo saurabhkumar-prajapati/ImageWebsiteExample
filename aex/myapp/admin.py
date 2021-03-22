9a92c318b0f13b799a8004c72d264440a0f95875
saurabhkumar-prajapati <sk43923@gmail.com>
2021-03-21 19:58:52 -0700

    first commit

--- aex/myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Newuser
-@admin.register(Newuser)
-class UserAdmin(admin.ModelAdmin):
-    list_display=('id','event_name','image','date','location')
